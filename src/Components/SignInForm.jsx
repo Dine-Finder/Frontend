52e649b12fcec6e5f0d9d33812785291938c4c41
BhargavNikumbh <72493359+BhargavNikumbh@users.noreply.github.com>
2024-07-13 11:17:07 +0100

    SignInForm and SignUpForm corrected

--- src/Components/SignInForm.jsx
+++ src/Components/SignInForm.jsx
@@ -18,13 +18,15 @@ function SignInForm() {
   const handleLogin = async (event) => {
     event.preventDefault();
     try {
-      const response = await axios.post(`${baseUrl}/login`, user);
+      console.log(`${baseUrl()}/login`);
+      const response = await axios.post(`${baseUrl()}/login`, user);
       // localStorage.setItem("access_token", response.data.access_token);
+      console.log(response);
       setAuth(response.data);
       setJwt(response.data.access_token);
       // Redirect to protected route
       console.log(response.data.user.roles);
-      const isAdmin = response.data.user.roles[0] == "Admin" ? true : false;
+      const isAdmin = response.data.user.roles[0] === "Admin" ? true : false;
       if (isAdmin) {
         console.log("Inside Admin");
         navigate("/admin", { replace: true });
